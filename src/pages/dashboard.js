714bbc79e2ffbf4a4296de0117c681fe035fc43b
May <skamreen@ggc.edu>
2021-03-21 15:33:41 -0400

    stuffz

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -9,43 +9,70 @@ const Dashboard = () => {
             style={{
                 display: 'flex',
                 justifyContent: 'Right',
-                alignItems: 'Right',
+                alignItems: 'Left',
                 height: '100vh',
                 color: '#343A40',
             }}
         >
-            <Table size="sm" className="ongoing">
-               
+           
+            <Table size="md" className="ongoing">
+                <h5>Ongoing</h5>
                
                 <tbody>
                     <tr>
                         
-                        <td>Task 1 fdsfsdfsdfsf</td>
+                        <td>fdsfsdfsdfsf</td>
 
                     </tr>
                     <tr>
                         
-                        <td>Task 1</td>
+                        <td>fdsfsdfsdfsf</td>
 
                     </tr>
                     <tr>
                         
-                        <td>Task 1</td>
+                        <td>fdsfsdfsdfsf</td>
 
                     </tr>
                     <tr>
                         
-                        <td>Task 1</td>
+                        <td>fdsfsdfsdfsf</td>
 
                     </tr>
-                    <tr>
-                     
-                        <td>Task 1</td>
-
-                    </tr>
+                   
 
                 </tbody>
             </Table>
+            <br/>
+            <Table size="md" className="completed">
+                <h5>Completed</h5>
+
+                <tbody>
+                    <tr>
+
+                        <td>fdsfsdfsdfsf</td>
+
+                    </tr>
+                    <tr>
+
+                        <td>fdsfsdfsdfsf</td>
+
+                    </tr>
+                    <tr>
+
+                        <td>fdsfsdfsdfsf</td>
+
+                    </tr>
+                    <tr>
+
+                        <td>fdsfsdfsdfsf</td>
+
+                    </tr>
+
+
+                </tbody>
+            </Table>
+
             
             </div>
   );
